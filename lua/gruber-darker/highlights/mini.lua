5118e049b0f2bee737575c7ef6e076949ad64f21
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2025-02-05 15:53:03 +0100

    up

--- lua/gruber-darker/highlights/mini.lua
+++ lua/gruber-darker/highlights/mini.lua
@@ -12,8 +12,8 @@ function M.setup()
     end
 end
 
-M.highlights["mini-pick-border"] = Highlight.new("MiniPickBorder", { fg = c["bg+4"], bg = c.none })
-M.highlights["mini-pick-normal"] = Highlight.new("MiniPickNormal", { fg = c.fg, bg = c.bg })
-M.highlights["mini-pick-preview-line"] = Highlight.new("MiniPickPreviewLine", { fg = c.fg, bg = c["bg+1"] })
+M.highlights["MiniPickBorder"] = Highlight.new("MiniPickBorder", { fg = c["bg+4"], bg = c.none })
+M.highlights["MiniPickNormal"] = Highlight.new("MiniPickNormal", { fg = c.fg, bg = c.bg })
+M.highlights["MiniPickPreviewLine"] = Highlight.new("MiniPickPreviewLine", { fg = c.fg, bg = c["bg+1"] })
 
 return M
